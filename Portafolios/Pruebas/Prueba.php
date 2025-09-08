edd8421a282339eb4e7b85f89fbcfb42872dce84
JoseEmiliano12 <2430284@upv.edu.mx>
2025-09-08 07:42:29 -0600

    Primera Version de caso 1

---
+++ Portafolios/Pruebas/Prueba.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>UPV - Ejemplo BAsico PHP</title>
+    </head>
+    <body>
+        <h1>UPV</h1>
+        <p>Hola Mundo</p>
+
+        <Button class="btn btn-primary">Enviar</Button>
+        <Button class="btn btn-primary">Enviar2</Button>
+    </body>
+</html>
---
+++ Portafolios/Pruebas/p1.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="es">
+    <meta charset="UTF-8">
+    <head>
+        <title>P1</title>
+        
